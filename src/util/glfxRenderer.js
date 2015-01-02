7af26d7ad356769fc3984f2b29b59a20e0f305cf
Thomas Boyt <me@thomasboyt.com>
2015-01-01 20:17:28 -0500

    Fix rendering to not cut off edges of canvas

--- src/util/glfxRenderer.js
+++ src/util/glfxRenderer.js
@@ -5,26 +5,56 @@
 
 var Coquette = require('coquette');
 
+/**
+ * Add padding around the initial game canvas so that the bulge'd canvas doesn't go outside the
+ * edge of the final canvas
+ */
+function addPadding(src, dest) {
+  var buf = 20;
+
+  dest.width = src.width + buf * 2;
+  dest.height = src.height + buf * 2;
+
+  var ctx = dest.getContext('2d');
+  ctx.drawImage(src, buf, buf);
+
+  return dest;
+}
+
+/**
+ * Monkey-patch the Coquette Renderer to add custom Glfx-related rendering
+ */
 function installGlfxRenderer() {
 
   var Renderer = Coquette.Renderer;
 
   Renderer.prototype._originalUpdate = Renderer.prototype.update;
 
+  /**
+   * Create padding and glfx canvases, remove original canvas from page, insert glfx canvas in its
+   * place
+   */
   Renderer.prototype.initGlfx = function() {
     var source = this.getCtx().canvas;
+
     this.glCanvas = fx.canvas();
 
     source.parentNode.insertBefore(this.glCanvas, source);
     source.style.display = 'none';
     this.glCanvas.id = source.id;
     source.id = 'old-' + source.id;
+
+    this.padCanvas = document.createElement('canvas');
   };
 
+  /**
+   * Render original canvas through padding canvas and gl canvas
+   */
   Renderer.prototype.update = function(interval) {
     this._originalUpdate(interval);
     var source = this.getCtx().canvas;
-    var texture = this.glCanvas.texture(source);
+    var padded = addPadding(source, this.padCanvas);
+    var texture = this.glCanvas.texture(padded);
 
     this.glCanvas.draw(texture)
       .bulgePinch(source.width / 2, source.height / 2, source.width * 0.8, 0.12)
